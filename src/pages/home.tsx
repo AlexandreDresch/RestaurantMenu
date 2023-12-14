6d4cde9ab0a9a5a8100d2111a926008798d5a379
AlexandreDresch <z3r0PC@protonmail.com>
2023-12-13 23:47:13 -0300

    feat: add home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col p-4 gap-4">
+            <Input placeholder="Procure por um prato"/>
+        </div>
+    )
+}
