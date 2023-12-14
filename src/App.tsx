0687f178658a20a88c125dbc111c105852428553
AlexandreDresch <z3r0PC@protonmail.com>
2023-12-13 23:44:34 -0300

    feat: intial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/ui/header";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
