afd360fa194d3b4f7724f6d2138aedda9a7c082b
AlexandreDresch <z3r0PC@protonmail.com>
2023-12-15 00:27:15 -0300

    feat: showing products in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,28 @@
+import HorizontalList from "@/components/ui/horizontal-list";
 import { Input } from "@/components/ui/input";
+import Link from "@/components/ui/link";
+import SectionTitle from "@/components/ui/section-title";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col p-4 gap-4">
-            <Input placeholder="Procure por um prato"/>
+  return (
+    <div className="flex flex-col p-4 gap-4">
+      <Input placeholder="Procure por um prato" />
+
+      <div className="flex flex-col gap-1">
+        <div className="flex justify-between">
+          <SectionTitle>Pratos</SectionTitle>
+          <Link target="/pratos" />
         </div>
-    )
+        <HorizontalList />
+      </div>
+      
+      <div className="flex flex-col gap-1">
+        <div className="flex justify-between">
+          <SectionTitle>Bebidas</SectionTitle>
+          <Link target="/bebidas" />
+        </div>
+        <HorizontalList />
+      </div>
+    </div>
+  );
 }
